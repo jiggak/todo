3993f9347d4835d7dc4f93f56a86da00a1acd441
Josh Kropf <josh@slashdev.ca>
2016-09-23 19:09:15 -0400

    hookup todo http request

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './todo.service';
 
 @Component({
    selector: 'todo-app',
-   template: '<h1>Hello, World!</h1>'
+   templateUrl: './app.component.html',
+   providers: [TodoService]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+   todos$;
+
+   constructor(private todoService: TodoService) { }
+
+   ngOnInit(): void {
+      this.todos$ = this.todoService.list();
+   }
+}
---
+++ client/src/app/todo.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
+import { Todo } from './todo';
+
+@Injectable()
+export class TodoService {
+   constructor(private http:Http) { }
+
+   list(): Observable<Todo> {
+      return this.http.get('/api/todo')
+         .map(res => res.json());
+   }
+}
